b5ce0747c76ca59b083f9e61a2514bf00f061821
choral7451 <chorales@naver.com>
2023-02-25 17:35:57 +0900

    Add. 3. 숫자야구

---
+++ 숫자야구/client.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import NumberBaseball from './NumberBaseball';
+import Test from "./RenderTest";
+
+
+ReactDOM.render(<NumberBaseball />, document.querySelector('#root'));
